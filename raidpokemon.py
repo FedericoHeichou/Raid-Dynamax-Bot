ddbdd8c581803752690f4e670f4ed0705ad21470
alessiocelentano <alessiocelentano2003@gmail.com>
2019-11-24 22:13:15 +0100

    General adjustments at the code

--- raidpokemon.py
+++ raidpokemon.py
@@ -4,10 +4,11 @@ import json
 import random
 
 
+
 with open('token.txt', 'r') as filee:
     token = filee.read().replace('\n', '')
 
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot('979765263:AAELCFhUsKZWyjnvwLuAowk8ZNSAHgRxa7k')
 user_dict = {}
 
 
@@ -26,122 +27,123 @@ class Raid():
 
 @bot.message_handler(commands=['start'])
 def home(message):
-    cid = message.chat.id
-    with open('texts.json', 'r') as filee:
-        texts = json.load(filee)
-
     try:
+        cid = message.chat.id
+
+        with open('texts.json', 'r') as filee:
+            texts = json.load(filee)
+
         text = ''
         if message.chat.type == 'private':
             text += texts["welcome1"]
         text += texts["welcome2"]
+
+        bot.send_message(cid, text, parse_mode='HTML')
+
     except Exception as e:
-        text = texts['error']
         bot.send_message(312012637, '`' + str(e) + '`', parse_mode='Markdown')
-
-
-    bot.send_message(cid, text, parse_mode='HTML')
 
 
 
 @bot.message_handler(commands=['add'])
 def yourfc(message):
-    cid = message.chat.id
-
-    with open('texts.json', 'r') as filee:
-        texts = json.load(filee)
-
     try:
-        with open('friendcodes.json', 'r') as filee:
-            data = json.load(filee)
+        cid = message.chat.id
 
-        if message.text == '/add' or message.text == '/add@RaidDynamaxBot':
-            bot.send_message(cid, texts["incomplete_fc_error"], parse_mode='HTML')
+        with open('texts.json', 'r') as filee:
+            texts = json.load(filee)
+
+        if message.chat.type == 'private':
+            text = texts['not_available']
+            bot.send_message(cid, text, parse_mode='HTML')
         else:
-            fc = message.text.replace('/add', '')
-            fc = fc.replace(' ', '')
-            if 'SW-' in fc:
-                fc = fc.replace('SW-', '')
+            with open('friendcodes.json', 'r') as filee:
+                data = json.load(filee)
+
+            if message.text == '/add' or message.text == '/add@RaidDynamaxBot':
+                bot.send_message(cid, texts["incomplete_fc_error"], parse_mode='HTML')
             else:
-                pass
-
-            check = fc
-            
-            for block in range(3):
-                if block != 0:
-                    if check[0] == '-':
-                        check = check.replace(check[0], '', 1)
-                    else:
-                        msg = bot.send_message(cid, texts["fc_error"])
-                        break
-
-                try:
-                    if msg:
-                        break
-                except Exception:
-                    pass
-
-                for num in range(4):
-                    if check[0] in '0123456789':
-                        check = check.replace(check[0], '', 1)
-                        continue
-                    else:
-                        msg = bot.send_message(cid, texts["fc_error"])
-                        break
-            
-                try:
-                    if msg:
-                        break
-                except Exception:
-                    pass
-
-            try:
-                if msg:
-                    pass
-            except Exception:
-                if message.chat.type != 'private' and check == '':
-                    if str(message.from_user.id) not in data[str(cid)]:
-                        text = texts['fc_add']
-
-                    data[str(cid)].update({message.from_user.id:{fc:message.from_user.first_name}})
-                    for idd in data[str(cid)]:
-                        if idd == str(message.from_user.id):
-                            try:
-                                if text:
-                                    pass
-                            except Exception:
-                                text = texts['fc_update']
-                            break
+                fc = message.text.replace('/add', '')
+                fc = fc.replace(' ', '')
+                if 'SW-' in fc:
+                    fc = fc.replace('SW-', '')
                 else:
-                    text = texts['not_available']
+                    pass
 
-                bot.send_message(cid, text)
+                check = fc
+                
+                for block in range(3):
+                    if block != 0:
+                        if check[0] == '-':
+                            check = check.replace(check[0], '', 1)
+                        else:
+                            msg = bot.send_message(cid, texts["fc_error"], parse_mode='HTML')
+                            break
 
-                with open('friendcodes.json', 'w') as filee:
-                    json.dump(data, filee, indent=4)
+                    try:
+                        if msg:
+                            break
+                    except Exception:
+                        pass
+
+                    for num in range(4):
+                        if check[0] in '0123456789':
+                            check = check.replace(check[0], '', 1)
+                            continue
+                        else:
+                            msg = bot.send_message(cid, texts["fc_error"], parse_mode='HTML')
+                            break
+                
+                    try:
+                        if msg:
+                            break
+                    except Exception:
+                        pass
+
+                try:
+                    if msg:
+                        pass
+                except Exception:
+                    if message.chat.type != 'private' and check == '':
+                        if str(message.from_user.id) not in data[str(cid)]:
+                            text = texts['fc_add']
+
+                        data[str(cid)].update({message.from_user.id:{fc:message.from_user.first_name}})
+                        for idd in data[str(cid)]:
+                            if idd == str(message.from_user.id):
+                                try:
+                                    if text:
+                                        pass
+                                except Exception:
+                                    text = texts['fc_update']
+                                break
+                    else:
+                        text = texts['not_available']
+
+                    bot.send_message(cid, text, parse_mode='HTML')
+
+                    with open('friendcodes.json', 'w') as filee:
+                        json.dump(data, filee, indent=4)
 
     except Exception as e:
-        text = texts['error']
-        bot.send_message(cid, text)
         bot.send_message(312012637, '`' + str(e) + '`', parse_mode='Markdown')
-
 
 
 
 @bot.message_handler(commands=['show'])
 def show_fc(message):
-    cid = message.chat.id
-
-    with open('texts.json', 'r') as filee:
-        texts = json.load(filee)
-
     try:
+        cid = message.chat.id
+
+        with open('texts.json', 'r') as filee:
+            texts = json.load(filee)
+
         with open('friendcodes.json', 'r') as filee:
             data = json.load(filee)
 
         if message.chat.type == 'private':
             text = texts['not_available']
-            bot.send_message(cid, text)
+            bot.send_message(cid, text, parse_mode='HTML')
         else:
             if str(cid) not in data:
                 bot.send_message(cid, texts['no_fcs'], parse_mode='HTML')
@@ -162,30 +164,27 @@ def show_fc(message):
                     bot.send_message(cid, texts['no_fcs'], parse_mode='HTML')
 
     except Exception as e:
-        text = texts['error']
-        bot.send_message(cid, text)
         bot.send_message(312012637, '`' + str(e) + '`', parse_mode='Markdown')
-
 
 
 
 @bot.message_handler(commands=['showme'])
 def show_my_fc(message):
-    cid = message.chat.id
-
-    with open('texts.json', 'r') as filee:
-        texts = json.load(filee)
-
     try:
+        cid = message.chat.id
+
+        with open('texts.json', 'r') as filee:
+            texts = json.load(filee)
+
         with open('friendcodes.json', 'r') as filee:
             data = json.load(filee)
 
         if message.chat.type == 'private':
             text = texts['not_available']
-            bot.send_message(cid, text)
+            bot.send_message(cid, text, parse_mode='HTML')
         else:
             if str(cid) not in data:
-                bot.send_message(cid, texts['no_fcs'], parse_mode='HTML')
+                bot.send_message(cid, texts['no_fc'], parse_mode='HTML')
             else:
                 for idd in data[str(cid)]:
                     if idd == str(message.from_user.id):
@@ -198,82 +197,80 @@ def show_my_fc(message):
                     bot.send_message(cid, text)
                 except UnboundLocalError:
                     bot.send_message(cid, texts['no_fc'], parse_mode='HTML')
+
     except Exception as e:
-        text = texts['error']
-        bot.send_message(cid, text)
         bot.send_message(312012637, '`' + str(e) + '`', parse_mode='Markdown')
-
-
-
 
 
 
 @bot.message_handler(commands=['new'])
 def new_raid(message):
-    cid = message.chat.id
-
-    with open('texts.json', 'r') as filee:
-        texts = json.load(filee)
-
     try:
-        with open('friendcodes.json', 'r') as filee:
-            data = json.load(filee)
+        cid = message.chat.id
 
-        raid = Raid()
-        user_dict[message.from_user.id] = raid
+        with open('texts.json', 'r') as filee:
+            texts = json.load(filee)
 
-        if message.text == '/new' or message.text == '/new@RaidDynamaxBot':
-            bot.send_message(cid, texts['new_raid_error'], parse_mode='HTML')
+        if message.chat.type == 'private':
+            text = texts['not_available']
+            bot.send_message(cid, text, parse_mode='HTML')
         else:
-            raid.idd = message.from_user.id
-            if '/new@RaidDynamaxBot ' in message.text:
-                raid.pokemon = message.text.replace('/new@RaidDynamaxBot ', '')
+            with open('friendcodes.json', 'r') as filee:
+                data = json.load(filee)
+
+            raid = Raid()
+            user_dict[message.from_user.id] = raid
+
+            if message.text == '/new' or message.text == '/new@RaidDynamaxBot':
+                bot.send_message(cid, texts['new_raid_error'], parse_mode='HTML')
             else:
-                raid.pokemon = message.text.replace('/new ', '')
-            raid.owner = message.from_user.first_name
-            try:
-                raid.fc = list(data[str(cid)][str(message.from_user.id)].keys())[0]
-            except Exception:
-                raid.fc = '-'
+                raid.idd = message.from_user.id
+                if '/new@RaidDynamaxBot ' in message.text:
+                    raid.pokemon = message.text.replace('/new@RaidDynamaxBot ', '')
+                else:
+                    raid.pokemon = message.text.replace('/new ', '')
+                raid.owner = message.from_user.first_name
+                try:
+                    raid.fc = list(data[str(cid)][str(message.from_user.id)].keys())[0]
+                except Exception:
+                    raid.fc = '-'
 
-            players = ['-', '-', '-']
-            n = 0
-            for i in range(len(raid.players)):
-                players[n] = raid.players[n]
-                n = n + 1
+                players = ['-', '-', '-']
+                n = 0
+                for i in range(len(raid.players)):
+                    players[n] = raid.players[n]
+                    n = n + 1
 
-            text = texts['new_raid'].format(
-                raid.pokemon,
-                raid.owner,
-                raid.fc,
-                players[0],
-                players[1],
-                players[2]
-            )
+                text = texts['new_raid'].format(
+                    raid.pokemon,
+                    raid.owner,
+                    raid.fc,
+                    players[0],
+                    players[1],
+                    players[2]
+                )
 
-            markup = types.InlineKeyboardMarkup()
-            join = types.InlineKeyboardButton('✅Partecipa✅', callback_data='join_raid'+str(raid.idd))
-            done = types.InlineKeyboardButton('🚫Chiudi🚫', callback_data='done'+str(raid.idd))
-            markup.row(join)
-            markup.row(done)
+                markup = types.InlineKeyboardMarkup()
+                join = types.InlineKeyboardButton('🙋‍♂️Partecipa🙋‍♂️', callback_data='join_raid'+str(raid.idd))
+                done = types.InlineKeyboardButton('🚫Chiudi🚫', callback_data='done'+str(raid.idd))
+                markup.row(join)
+                markup.row(done)
 
-            bot.send_message(cid, text, parse_mode='HTML', reply_markup=markup)
+                bot.send_message(cid, text, parse_mode='HTML', reply_markup=markup)
 
     except Exception as e:
-        text = texts['error']
-        bot.send_message(cid, text)
         bot.send_message(312012637, '`' + str(e) + '`', parse_mode='Markdown')
 
 
 
 @bot.callback_query_handler(func=lambda call: 'join_raid' in call.data)
 def join(call):
-    cid = call.message.chat.id
-
-    with open('texts.json', 'r') as filee:
-        texts = json.load(filee)
-
     try:
+        cid = call.message.chat.id
+
+        with open('texts.json', 'r') as filee:
+            texts = json.load(filee)
+
         mid = call.message.message_id
         raid = user_dict[int(call.data.replace('join_raid', ''))]
 
@@ -311,24 +308,22 @@ def join(call):
                 markup.row(done)
 
                 bot.edit_message_text(text, cid, mid, reply_markup=markup, parse_mode='HTML')
+
     except Exception as e:
-        text = texts['error']
-        bot.send_message(cid, text)
         bot.send_message(312012637, '`' + str(e) + '`', parse_mode='Markdown')
 
 
 
 @bot.callback_query_handler(func=lambda call: 'done' in call.data)
 def done(call):
-    cid = call.message.chat.id
-
-    with open('texts.json', 'r') as filee:
-        texts = json.load(filee)
-
     try:
+        cid = call.message.chat.id
+
+        with open('texts.json', 'r') as filee:
+            texts = json.load(filee)
+
         mid = call.message.message_id
         raid = user_dict[int(call.data.replace('done', ''))]
-
 
         if call.from_user.id == raid.idd:
             markup = types.InlineKeyboardMarkup()
@@ -339,22 +334,20 @@ def done(call):
             bot.edit_message_reply_markup(cid, mid, reply_markup=markup)
         else:
             bot.answer_callback_query(call.id, texts['not_creator'], True)
-    except Exception as e:
-        text = texts['error']
-        bot.send_message(cid, text)
-        bot.send_message(312012637, '`' + str(e) + '`', parse_mode='Markdown')
 
+    except Exception as e:
+        bot.send_message(312012637, '`' + str(e) + '`', parse_mode='Markdown')
 
 
 
 @bot.callback_query_handler(func=lambda call: 'yes' in call.data)
 def confirm(call):
-    cid = call.message.chat.id
-
-    with open('texts.json', 'r') as filee:
-        texts = json.load(filee)
-
     try:
+        cid = call.message.chat.id
+
+        with open('texts.json', 'r') as filee:
+            texts = json.load(filee)
+
         mid = call.message.message_id
         raid = user_dict[int(call.data.replace('yes', ''))]
 
@@ -387,21 +380,20 @@ def confirm(call):
             bot.edit_message_text(text, cid, mid, reply_markup=markup, parse_mode='HTML')
         else:
             bot.answer_callback_query(call.id, texts['not_creator'], True)
+
     except Exception as e:
-        text = texts['error']
-        bot.send_message(cid, text)
         bot.send_message(312012637, '`' + str(e) + '`', parse_mode='Markdown')
 
 
 
 @bot.callback_query_handler(func=lambda call: 'no' in call.data)
 def back(call):
-    cid = call.message.chat.id
-
-    with open('texts.json', 'r') as filee:
-        texts = json.load(filee)
-
     try:
+        cid = call.message.chat.id
+
+        with open('texts.json', 'r') as filee:
+            texts = json.load(filee)
+
         mid = call.message.message_id
         raid = user_dict[int(call.data.replace('no', ''))]
 
@@ -430,9 +422,8 @@ def back(call):
             bot.edit_message_text(text, cid, mid, reply_markup=markup, parse_mode='HTML')
         else:
             bot.answer_callback_query(call.id, texts['not_creator'], True)
+
     except Exception as e:
-        text = texts['error']
-        bot.send_message(cid, text)
         bot.send_message(312012637, '`' + str(e) + '`', parse_mode='Markdown')
 
 
@@ -449,25 +440,24 @@ def password(call):
             bot.answer_callback_query(call.id, raid.pin, True)
         else:
             bot.answer_callback_query(call.id, texts["not_player"], True)
+
     except Exception as e:
         bot.send_message(312012637, '`' + str(e) + '`', parse_mode='Markdown')
 
 
 
-
 @bot.message_handler(commands=['credits'])
 def credits(message):
-    cid = message.chat.id
-
-    with open('texts.json', 'r') as filee:
-        texts = json.load(filee)
-
     try:
+        cid = message.chat.id
+
+        with open('texts.json', 'r') as filee:
+            texts = json.load(filee)
+
         text = texts['credits']
         bot.send_message(cid, text)
+
     except Exception as e:
-        text = texts['error']
-        bot.send_message(cid, text)
         bot.send_message(312012637, '`' + str(e) + '`', parse_mode='Markdown')
 
 
